8e64be9b342faca8e682f02c60b691c07db8f472
Daniel Anifowoshe <danielanifowoshe04@gmail.com>
2025-07-19 17:16:01 +0100

    Implement InputField component with state management for todo input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './styles/InputFieldCss.css'
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+
+const HandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setTodo(event.target.value);
+}
+
   return (
     <form className='input'>
-      <input type="input" placeholder='Enter a task' className='input_box' />
+      <input type="input" placeholder='Enter a task' className='input_box' value={todo} onChange={HandleChange} />
+      <button className="input_submit" type='submit'>Go</button>
     </form>
   )
 }
