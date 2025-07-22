a42b0eec3eb4cde4317337ffe04eb557e3773879
Daniel Anifowoshe <danielanifowoshe04@gmail.com>
2025-07-22 14:23:06 +0100

    Add TodoList and update InputField to handle todo submission
    - Implement TodoList component to display todos
    - Update InputField to include handleAdd function for form submission
    - Introduce Todo interface for type safety

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles/InputFieldCss.css'
 
 interface Props {
   todo: string,
   setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (event: React.FormEvent) => void
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
 
 const HandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   setTodo(event.target.value);
 }
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='input'>
-      <input type="input" placeholder='Enter a task' className='input_box' value={todo} onChange={HandleChange} />
+    <form className='input' onSubmit={
+      (event) => {
+        handleAdd(event);
+        inputRef.current?.blur();
+      }
+    }>
+      <input
+        ref={inputRef} 
+        type="input" 
+        placeholder='Enter a task' className='input_box' 
+        value={todo} 
+        onChange={HandleChange} />
       <button className="input_submit" type='submit'>Go</button>
     </form>
   )
